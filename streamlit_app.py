7a9c195be97cf353f7c76a3c73349fd330001ce8
DHOURA <ahmed.dhoura@sbi-group.com>
2026-08-12 11:28:00 +0100

    Fix smoothie order success message and response display
    Uncomment success message and update smoothie response handling.

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,8 +46,9 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         #st.success('Your Smoothie is ordered!', icon="✅")
         #st.success('Your Smoothie is ordered,', name_on_order, '!')
-        st.success(f'Your Smoothie is ordered, {name_on_order}!'
+        st.success(f'Your Smoothie is ordered, {name_on_order}!')
 
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
